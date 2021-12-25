bf8fe0bbee3c16d6a319fd323a129c2e738ea7d9
ahmet.avci <ahmet.avci@trendyol.com>
2021-12-25 16:56:58 +0300

    add couchbase repository

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/ahmetavc/wallet/pkg/infra"
 
 func main() {
+	_ = infra.NewCouchbaseRepository()
 	infra.SetupRouter()
 }
---
+++ pkg/infra/db.go
@@ -0,0 +1,84 @@
+package infra
+
+import (
+	"github.com/couchbase/gocb/v2"
+	"time"
+)
+
+type Repository interface {
+	Upsert(key string, value interface{}) error
+	Get(key string) (interface{}, error)
+	Remove(key string) error
+	Close() error
+}
+
+type CouchbaseRepository struct {
+	collection *gocb.Collection
+	cluster    *gocb.Cluster
+}
+
+func NewCouchbaseRepository() *CouchbaseRepository {
+	//couchbaseHost := os.Getenv("COUCHBASE_HOST")
+	couchbaseHost := "localhost:8091"
+	cluster, err := gocb.Connect(couchbaseHost, gocb.ClusterOptions{
+		Username: "Administrator",
+		Password: "password",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	bucket := cluster.Bucket("wallet")
+
+	err = bucket.WaitUntilReady(5*time.Second, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	collection := bucket.DefaultCollection()
+
+	return &CouchbaseRepository{collection: collection, cluster: cluster}
+}
+
+func (repository *CouchbaseRepository) Upsert(key string, value interface{}) error {
+	_, err := repository.collection.Upsert(key, value, &gocb.UpsertOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repository *CouchbaseRepository) Get(key string) (interface{}, error) {
+	data, err := repository.collection.Get(key, &gocb.GetOptions{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var content interface{}
+	if err := data.Content(&content); err != nil {
+		panic(err)
+	}
+
+	return content, nil
+}
+
+func (repository *CouchbaseRepository) Remove(key string) error {
+	_, err := repository.collection.Remove(key, &gocb.RemoveOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repository *CouchbaseRepository) Close() error {
+	if err := repository.cluster.Close(&gocb.ClusterCloseOptions{}); err != nil {
+		return err
+	}
+
+	return nil
+}
